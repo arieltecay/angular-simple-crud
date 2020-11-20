f2d10bc360370165119a349370c81ea49ef358b8
arieltecay <arieltecay@gmail.com>
2020-11-20 19:34:54 -0300

    Actualización del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Players  } from './models/Players';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  playerFootball: Players[] = [
+    {id: 1, name: 'Roberto Perfumo', posicion: 'defensor'},
+    {id: 2, name: 'Alfio Basile', posicion: 'defensor'},
+    {id: 3, name: 'Humberto Maschio', posicion: 'defensor'}
+  ];
 }
